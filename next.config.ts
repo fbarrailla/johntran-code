19bd2776a57422178acaa1c2be9b55aeb7c31ed4
Francois Barrailla <francois.barrailla@gmail.com>
2026-03-23 19:09:49 +0700

    change basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: "/johntran-code",
   images: {
     unoptimized: true,
   },
